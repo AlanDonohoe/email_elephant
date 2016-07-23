a9906f4225a776d5b1c7022d9e9fe3196a6de3bd
Alan Donohoe <alandonohoe@hotmail.com>
2016-07-23 18:32:22 +0100

    add validations to emails and sites urls

--- app/models/email.rb
+++ app/models/email.rb
@@ -1,3 +1,5 @@
 class Email < ApplicationRecord
   belongs_to :site, optional: true
+  validates_format_of :address, with: /\A\s*([^@\\s]{1,64})@((?:[-\p{L}\d]+\.)+\p{L}{2,})\s*\z/, message: "Invalid email format"
+  validates :address, uniqueness: true
 end
--- app/models/site.rb
+++ app/models/site.rb
@@ -1,4 +1,6 @@
 class Site < ApplicationRecord
   has_many :emails
   accepts_nested_attributes_for :emails
+  validates_format_of :url, with: /^(https?:\/\/)([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/, message: "Invalid URL format"
+  validates :url, uniqueness: true
 end
